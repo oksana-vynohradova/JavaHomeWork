7565839dd43a1c1bbb45fecfb3840d3ccb408ebc
oksana-vynohradova <oksana.vynohradova@privatbank.ua>
2021-11-21 15:41:28 +0200

    hw9 commit

---
+++ src/com/pb/vynohradova_oksana/hw9/FileNumbers.java
@@ -0,0 +1,81 @@
+package com.pb.vynohradova_oksana.hw9;
+
+import java.io.FileWriter;
+import java.io.Writer;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class FileNumbers {
+    public static void main(String[] args) {
+        String str = createNumbersFile();
+        System.out.println("Создан файл " + str);
+
+        String sb = createOddNumbersFile(str);
+        System.out.println("Создан файл " + sb);
+    }
+
+    static String createNumbersFile() {
+        StringBuilder sb = new StringBuilder();
+        Random rand = new Random();
+
+        for (int j = 0; j < 10; j++) {
+            for (int i = 0; i < 10; i++) {
+                sb.append(rand.nextInt(100));
+                if (i != 9) {
+                    sb.append(" ");
+                } else if (j != 9) {
+                    sb.append(System.lineSeparator());
+                }
+            }
+        }
+
+        String fileName = "files\\numbers.txt";
+        try (Writer writer = new FileWriter(fileName)){
+            writer.write(sb.toString());
+        } catch (Exception e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
+
+       return fileName;
+    }
+
+    static String createOddNumbersFile(String fileName) {
+        StringBuilder sb = new StringBuilder();
+        Path path = Paths.get(fileName);
+
+        try (Scanner scan = new Scanner(path)) {
+            while (scan.hasNextLine()) {
+                String[] str =  scan.nextLine().split(" ");
+                for (int j = 0; j < str.length; j++) {
+                    int x = Integer.parseInt(str[j]);
+                    if (x%2 != 0) {
+                        sb.append(x);
+                    } else {
+                        sb.append(0);
+                    }
+
+                    if (j != 9) {
+                        sb.append(" ");
+                    } else {
+                        sb.append(System.lineSeparator());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        sb.delete(sb.length() - 2,sb.length());
+
+        try (Writer writer = new FileWriter("files\\odd-numbers.txt")){
+            writer.write(sb.toString());
+        } catch (Exception e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
+
+        return "files\\odd-numbers.txt";
+    }
+}
